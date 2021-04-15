52b760a562a26e510365ef2dc9dc7a574d40e5cf
JKhrist <khrist.getonnet.agency>
2021-04-15 12:19:54 +0800

    first commit

---
+++ includes/class-woo-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       woo.com
+ * @since      1.0.0
+ *
+ * @package    Woo
+ * @subpackage Woo/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Woo
+ * @subpackage Woo/includes
+ * @author     woo <woo@gmail.com>
+ */
+class Woo_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-woo-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       woo.com
+ * @since      1.0.0
+ *
+ * @package    Woo
+ * @subpackage Woo/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Woo
+ * @subpackage Woo/includes
+ * @author     woo <woo@gmail.com>
+ */
+class Woo_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
